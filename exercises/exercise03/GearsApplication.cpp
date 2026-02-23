600b9403fea49db23ccac78ab91e2bb97bf429b1
ptso <ptso@itu.dk>
2026-02-23 17:30:42 +0100

    Solved exercise 3.1

--- exercises/exercise03/GearsApplication.cpp
+++ exercises/exercise03/GearsApplication.cpp
@@ -52,8 +52,12 @@ void GearsApplication::Render()
 
 
     // (todo) 03.1: Draw large gear at the center
+    float angle = 1.0f * GetCurrentTime();
+    glm::vec3 axis = glm::vec3(0.0f, 0.0f, 1.0f);
+    glm::mat4 rotationMatrix = glm::rotate(angle, axis);
+
     glm::mat4 centerGearMatrix(1.0f);
-    DrawGear(m_largeGear, centerGearMatrix, Color(1.0f, 1.0f, 1.0f));
+    DrawGear(m_largeGear, rotationMatrix, Color(1.0f, 1.0f, 1.0f));
 
     // (todo) 03.2: Draw medium gear to the right
 
@@ -96,7 +100,7 @@ void GearsApplication::InitializeShaders()
     m_colorUniform = m_shaderProgram.GetUniformLocation("Color");
 
     // (todo) 03.1: Find the WorldMatrix uniform location
-
+    m_worldMatrixUniform = m_shaderProgram.GetUniformLocation("WorldMatrix");
 
     // (todo) 03.5: Find the ViewProjMatrix uniform location
 
@@ -108,8 +112,8 @@ void GearsApplication::DrawGear(const Mesh& mesh, const glm::mat4& worldMatrix, 
 {
     m_shaderProgram.SetUniform(m_colorUniform, static_cast<glm::vec3>(color));
 
-    // (todo) 03.1: Set the value of the WorldMatrix uniform
-
+    // 03.1: Set the value of the WorldMatrix uniform
+    m_shaderProgram.SetUniform(m_worldMatrixUniform, worldMatrix);
 
     mesh.DrawSubmesh(0);
 }
